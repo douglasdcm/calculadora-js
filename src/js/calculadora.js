4d0ea03597f5a187d43b49808cb5f1e474482034
Douglas Cardoso <douglas.dcm@gmail.com>
2019-04-05 01:11:53 -0300

    implemtação da refatoracao

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,41 +1,51 @@
 var Calculadora = {
 	adicionar: function(num1, num2){
-		if(isNaN(num1) || isNaN(num2)){
-			return 0;
-		}
-		num1 = parseFloat(num1);
-		num2 = parseFloat(num2);
-		return num1 + num2;
+		return calcular(num1, num2, 'ADICAO');
 	},
 
-	subtrair: function(num1, num2){
-		if(isNaN(num1) || isNaN(num2)){
-			return 0;
-		}
-		return num1 - num2;
+	subtrair: function(num1, num2){ 
+		return calcular(num1, num2, 'SUBTRACAO');
 	},
 
-	dividir: function(num1, num2){
-		if(isNaN(num1) || isNaN(num2)){
-			return 0;
-		};
+	dividir: function(num1, num2){		
 
 		if(num2 == 0){
 			return 'ERROR';
 		};
 
-		return num1 / num2;
+		return calcular(num1, num2, 'divisao');
 	},
 
 	multiplicar: function(num1, num2){
-		if(isNaN(num1) || isNaN(num2)){
-			return 0;
-		}
-		return (num1 * num2);
+
+		return calcular(num1, num2, 'multiplicacao');
 	}
 };
 
 //usado na funcao 'require' do NodeJS
 if(typeof module !== 'undefined' && typeof module.exports !== 'undefinde'){
 	module.exports = Calculadora;
+}
+
+function calcular (num1, num2, operacao){
+	if(isNaN(num1) || isNaN(num2)){
+		return 0;
+	};
+	num1 = parseFloat(num1);
+	num2 = parseFloat(num2);
+	switch(operacao.toUpperCase()){
+		case 'ADICAO':
+			valor = num1 + num2;
+			break;
+		case 'SUBTRACAO':
+			valor = num1 - num2;
+			break;
+		case 'MULTIPLICACAO':
+			valor = num1 * num2;
+			break;
+		case 'DIVISAO':
+			valor = num1 / num2;
+			break;
+	}
+	return valor;
 }
